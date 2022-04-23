ba07f3ea2317b2dc0b41256a686b5090f0a2d3e4
Noah Lanz <noah@oliverlanz.ch>
2022-04-23 15:16:35 +0200

    Can play sound. But only on Windows.

--- Program.cs
+++ Program.cs
@@ -2,9 +2,17 @@
 Console.WriteLine("Prut Program");
 Console.WriteLine("************************************************");
 
-Console.WriteLine("Doing stuff");
+Console.WriteLine("Creating media player...");
+var mp = new System.Media.SoundPlayer();
 
+Console.WriteLine("Loading sound file...");
+var soundFile = "C:\\Users\\noah\\Development\\prut\\sounds\\reverbing_fart.wav";
+mp.SoundLocation = soundFile;
+mp.Load();
 
-Console.WriteLine("Pree  [Enter] to close...");
+Console.WriteLine("Playing sound...");
+mp.Play();
+
+Console.WriteLine("Press [Enter] to close...");
 
 Console.ReadLine();
