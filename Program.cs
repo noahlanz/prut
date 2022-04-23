76cfa142324c0c6a814e9c70d3713f6f31db6cf9
Noah Lanz <noah@oliverlanz.ch>
2022-04-23 15:47:08 +0200

    Made independent of local path

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("************************************************");
 Console.WriteLine("Prut Program");
 Console.WriteLine("************************************************");
 
-var soundFile = "C:\\Users\\noah\\Development\\prut\\sounds\\reverbing_fart.wav";
+var soundFile = "sounds/reverbing_fart.wav";
 
 Console.WriteLine("Playing sound at: " + soundFile);
 new FartPlayer(soundFile).Play();
